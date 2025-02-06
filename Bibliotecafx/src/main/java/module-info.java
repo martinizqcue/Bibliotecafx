936dd8d6a407ae417ec8a0c5de4e1ae3a4c9c503
Martin <martinizquierdocuevas@gmail.com>
2025-02-06 20:59:06 +0100

    No me va al pulsar el boton de gestionLibro

--- Bibliotecafx/src/main/java/module-info.java
+++ Bibliotecafx/src/main/java/module-info.java
@@ -15,5 +15,8 @@ module org.example.bibliotecafx {
     requires java.naming;
 
     opens org.example.bibliotecafx to javafx.fxml;
+    opens org.example.bibliotecafx.entities to org.hibernate.orm.core, javafx.fxml;
+
     exports org.example.bibliotecafx;
+    exports org.example.bibliotecafx.entities;
 }
